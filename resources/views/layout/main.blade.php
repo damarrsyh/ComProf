8b6e213ef8f936071263c905dbb8305f3c710a79
damarrnsyh <ramadiansyahdamar@gmail.com>
2024-01-05 16:14:30 +0700

    update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Mitra Usaha Mandiri</title>
+    <title>KSPMUM - Finance For The Poor</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="/assets/media/favicon.ico" />
     <!-- Bootstrap Icons-->
