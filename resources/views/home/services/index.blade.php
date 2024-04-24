4018face1bdf6cff58e5abfbc85d82c0c0c5c638
damarrnsyh <ramadiansyahdamar@gmail.com>
2024-04-24 16:09:09 +0700

    update new services

--- resources/views/home/services/index.blade.php
+++ resources/views/home/services/index.blade.php
@@ -1,56 +1,121 @@
 <section class="page-section" id="services">
   <div class="container" data-aos="fade-in" data-aos-duration="1000">
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-4" style="background: var(--bs-gray-200); border-radius: 15px 0px 0px 15px;">
         <div class="overlay">
-          <img src="/assets/media/img/mum/services.jpg" class="img-fluid" alt="Gambar"
+          <img src="/assets/media/img/mum/services.jpeg" class="img-fluid" alt="Gambar"
             style="border-radius: 15px 0px 0px 15px;">
-          <div class="overlay-content">
-            <h1>Our Services</h1>
-            <p>Products</p>
+        </div>
+      </div>
+      <div class="col-md-8 p-4" style="background: var(--bs-gray-200); border-radius: 0px 15px 15px 0px;">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="col align-vertical">
+              <img src="assets/media/img/services/1.png" class="img-fluid my-3" alt="Responsive Image" width="70px">
+            </div>
+            <div class="col align-vertical">
+              <h5 class="fw-bold my-3">PEMBIAYAAN SISTEM GRAMEEN BANK</h5>
+              <p class="text-muted">Metode Grameen Bank merupakan program penyaluran kredit mikro yang ditujukan bagi
+                golongan masyarakat miskin di pedesaan.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="col align-vertical">
+              <img src="assets/media/img/services/2.png" class="img-fluid my-3" alt="Responsive Image" width="70px">
+            </div>
+            <div class="col align-vertical">
+              <h5 class="fw-bold my-3">PELAYANAN KESEHATAN UNTUK PEREMPUAN</h5>
+              <p class="text-muted">Memberikan pelayanan dan menjamin kesehatan pada perempuan yang menjadi anggota
+                koperasi Mitra Usaha Mandiri</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="col align-vertical">
+              <img src="assets/media/img/services/3.png" class="img-fluid my-3" alt="Responsive Image" width="70px">
+            </div>
+            <div class="col align-vertical">
+              <h5 class="fw-bold my-3">SIMPANAN SUKARELA</h5>
+              <p class="text-muted">Simpanan sukarela adalah iuran yang dibayar oleh anggota koperasi secara sukarela
+                atau tidak ada paksaan
+                dimana besarnya iuran sesuai dengan kemampuan masing-masing anggota.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="col align-vertical">
+              <img src="assets/media/img/services/4.png" class="img-fluid my-3" alt="Responsive Image" width="70px">
+            </div>
+            <div class="col align-vertical">
+              <h5 class="fw-bold my-3">PEMBIAYAAN KHUSUS</h5>
+              <p class="text-muted">Memberikan pembiayaan khusus untuk perempuan yang membutuhkan modal usaha untuk
+                mencukupi kehidupannya.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="col align-vertical">
+              <img src="assets/media/img/services/5.png" class="img-fluid my-3" alt="Responsive Image" width="70px">
+            </div>
+            <div class="col align-vertical">
+              <h5 class="fw-bold my-3">SIMPANAN BERJANGKA (SIMBA)</h5>
+              <p class="text-muted">Simpanan Berjangka adalah simpanan di koperasi yang penyetorannya dilakukan sekali
+                dan penarikannya hanya
+                dapat dilakukan pada waktu tertentu menurut perjanjian antara penyimpan dengan koperasi yang
+                bersangkutan.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="col align-vertical">
+              <img src="assets/media/img/services/7.png" class="img-fluid my-3" alt="Responsive Image" width="70px">
+            </div>
+            <div class="col align-vertical">
+              <h5 class="fw-bold my-3">PELAYANAN PENGEMBANGAN WIRAUSAHA PEREMPUAN</h5>
+              <p class="text-muted">Memberikan pelayanan untuk mengembangkan kewirausahaan setiap perempuan yang menjadi
+                anggota koperasi Mitra
+                Usaha Mandiri</p>
+            </div>
           </div>
         </div>
       </div>
-      <div class="col-md-4" style="background: var(--bs-gray-200);">
-        <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal1">
-          <div class="text-center mb-2"><img src="assets/media/img/services/1.png" width="90px"></div>
-          <hr class="divider">
+      {{-- <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal1">
+        <div class="text-center mb-2"><img src="assets/media/img/services/1.png" width="90px">
           <h3 class="h4 text-center mb-2 fw-bold px-6">PEMBIAYAAN SISTEM GRAMEEN BANK</h3>
         </div>
-        <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal2">
-          <div class="text-center mb-2"><img src="assets/media/img/services/4.png" width="90px"></div>
-          <hr class="divider">
-          <h3 class="h4 text-center mb-2 fw-bold px-6">PEMBIAYAAN KHUSUS</h3>
-        </div>
-        <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal3">
-          <div class="text-center mb-2"><img src="assets/media/img/services/5.png" width="90px"></div>
-          <hr class="divider">
-          <h3 class="h4 text-center mb-2 fw-bold px-6">SIMPANAN BERJANGKA (SIMBA)</h3>
-        </div>
+        <hr class="divider">
+      </div>
+      <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal2">
+        <div class="text-center mb-2"><img src="assets/media/img/services/4.png" width="90px"></div>
+        <hr class="divider">
+        <h3 class="h4 text-center mb-2 fw-bold px-6">PEMBIAYAAN KHUSUS</h3>
+      </div>
+      <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal3">
+        <div class="text-center mb-2"><img src="assets/media/img/services/5.png" width="90px"></div>
+        <hr class="divider">
+        <h3 class="h4 text-center mb-2 fw-bold px-6">SIMPANAN BERJANGKA (SIMBA)</h3>
+      </div>
       </div>
       <div class="col-md-4" style="border-radius: 0px 15px 15px 0px; background: var(--bs-gray-200);">
-        <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal4">
-          <div class="text-center mb-2"><img src="assets/media/img/services/3.png" width="90px"></div>
-          <hr class="divider">
-          <h3 class="h4 text-center mb-2 fw-bold px-6">SIMPANAN SUKARELA</h3>
-        </div>
-        <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal5">
-          <div class="text-center mb-2"><img src="assets/media/img/services/2.png" width="90px"></div>
-          <hr class="divider">
-          <h3 class="h4 text-center mb-2 fw-bold px-6">PELAYANAN KESEHATAN UNTUK PEREMPUAN</h3>
-        </div>
-        <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal6">
-          <div class="text-center mb-2"><img src="assets/media/img/services/7.png" width="90px"></div>
-          <hr class="divider">
-          <h3 class="h4 text-center mb-2 fw-bold px-6">PELAYANAN PENGEMBANGAN WIRAUSAHA PEREMPUAN</h3>
-        </div>
+      <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal4">
+        <div class="text-center mb-2"><img src="assets/media/img/services/3.png" width="90px"></div>
+        <hr class="divider">
+        <h3 class="h4 text-center mb-2 fw-bold px-6">SIMPANAN SUKARELA</h3>
       </div>
+      <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal5">
+        <div class="text-center mb-2"><img src="assets/media/img/services/2.png" width="90px"></div>
+        <hr class="divider">
+        <h3 class="h4 text-center mb-2 fw-bold px-6">PELAYANAN KESEHATAN UNTUK PEREMPUAN</h3>
+      </div>
+      <div class="mt-5 hov-1" data-bs-toggle="modal" data-bs-target="#Modal6">
+        <div class="text-center mb-2"><img src="assets/media/img/services/7.png" width="90px"></div>
+        <hr class="divider">
+        <h3 class="h4 text-center mb-2 fw-bold px-6">PELAYANAN PENGEMBANGAN WIRAUSAHA PEREMPUAN</h3>
+      </div> --}}
     </div>
   </div>
 </section>
 
 <!-- MODAL -->
-<div class="modal fade" id="Modal1" tabindex="-1" aria-labelledby="ModalLabel1" aria-hidden="true">
+{{-- <div class="modal fade" id="Modal1" tabindex="-1" aria-labelledby="ModalLabel1" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
@@ -144,5 +209,5 @@
       </div>
     </div>
   </div>
-</div>
+</div> --}}
 <!-- END MODAL -->
